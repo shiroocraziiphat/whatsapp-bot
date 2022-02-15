3bb40b7a5f597777907658978a9e92b724f21520
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-02-16 04:27:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281284760551', '6281284760551'] // Your Number
+global.owner = ['6282361160044', '6282361160044'] // Your Number
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -34,16 +34,16 @@ global.APIKeys = { // APIKey in here
 
 
 // Sticker WM
-global.packname = '© erza bot'
-global.author = 'ig @_sekhaa'
+global.packname = '©Shiro botz'
+global.author = 'lovvvyuuu <3'
 
-global.wm = '© reysekha'
-global.wait = '⏳ tunggu permintaan kamu sedang di proses...'
+global.wm = '©Shiro Botz'
+global.wait = ' *tunggu permintaan kamu sedang di proses...* '
 global.eror = '× Upss Erorr silahkan lapor owner'
 global.benar = '✓'
 global.salah = '×'
 global.dikit = 'hampir benar'
-global.footer = '© sekha'
+global.footer = '©shiri'
 global.lw = 'https://api.reysekha.xyz/api/textpro/greenhoror?apikey=apirey&text='
 global.reyganz= 'https://api.reysekha.xyz/api/wallpaper/ukhty?apikey='
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
